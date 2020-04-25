de596c07879e3a5a5f40514ae19f582aafdf7add
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-04-26 02:28:16 +0530

    4.4: helper functions and unit tests, step2

--- part4/Blog-List/utils/list_helper.js
+++ part4/Blog-List/utils/list_helper.js
@@ -1,5 +1,17 @@
 const dummy =(blogs) => {
     return 1
 }
+const totalLikes=(blogs) => {
+    if(blogs.length === 0){
+        return 0
+    }
+    else if(blogs.length === 1){
+        return blogs[0].likes
+    }
+    const totalLikes=blogs.reduce((total,blogItem) => {
+        return total + blogItem.likes
+    },0)
 
-module.exports={dummy}
+    return totalLikes
+}
+module.exports={dummy,totalLikes}
