2e7dfbad671cbbecc78d7810def5ba50b6b32524
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-05-05 17:26:17 +0530

    1.5: course information, step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,29 +10,30 @@ const Content=(props) => (
     <Part part={props.part[2]}/>
   </div>
 )
-const Footer=({total}) => <p>Number of exercises {total}</p>
+const Footer=({parts}) => <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 const App=() => {
-  const course='Half Stack Application Development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return(
     <div>
-      <Header course={course}/>
-      <Content part={parts}/>
-      <Footer total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header course={course.name}/>
+      <Content part={course.parts}/>
+      <Footer parts={course.parts}/>
     </div>
   )
 }
