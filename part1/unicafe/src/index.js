348fdce15e6e386a61eb358779ca81f826b92e6f
GRajesh1198 <50822370+GRajesh1198@users.noreply.github.com>
2020-05-06 07:49:42 +0530

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,16 +6,11 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total,setTotal]=useState(0)
 
-  const goodHandler=()=>{
-    setGood(good + 1)
-  }
-  const neutralHandler=()=>{
-    setNeutral(neutral + 1)
-  }
-  const badHandler=()=>{
-    setBad(bad + 1)
-  }
+  const goodHandler=()=>{setTotal(total+1);setGood(good + 1)}
+  const neutralHandler=()=>{setTotal(total+1);setNeutral(neutral + 1)}
+  const badHandler=()=>{setTotal(total+1);setBad(bad + 1)}
 
   return (
     <div>
@@ -27,6 +22,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad)/total}</p>
+      <p>positive {(good/total)*100}%</p>
     </div>
   )
 }
